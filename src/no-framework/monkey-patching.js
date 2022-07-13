d9ceacd57872e432c3f225ff8db5d70801409ef9
CaseyCameron <mcasey.cameron@gmail.com>
2022-07-13 14:32:25 -0700

    monkey-patching definition & example

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,3 +2,16 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+// originalGetWinner servers as a cleanup function. Before every test, it resets.
+// this is called monkey-patching
+const originalGetWinner = utils.getWinner
+// mock out a function to ensure getWinner always returns player 1
+// this makes our test deterministic
+utils.getWinner = (p1, p2) => p1
+
+// mocks out a random winner so it will pass or fail w/o the functionality above
+const winner = thumbWar('Kent C. Dodds', 'Kent Wheeler')
+assert.strictEqual(winner, 'Kent C. Dodds')
+
+// clean up after the test so other tests in the module can use it in its unmodified state
+utils.getWinner = originalGetWinner
